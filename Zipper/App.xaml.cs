553d09e9b88aeb82e277d7d1ccf5ea908afe1321
Miguel Rivas Méndez <migueladanrm@outlook.com>
2018-05-14 20:35:06 -0600

    Preparación inicial del proyecto

---
+++ Zipper/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Zipper
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Arranque de la aplicación.
+        /// </summary>
+        /// <param name="e">Argumentos de arranque.</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+        }
+    }
+}
